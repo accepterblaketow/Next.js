6f8b73daaa571ecaa58f228e22424e48fc85ae10
RoseNFan <48249004+accepterblaketow@users.noreply.github.com>
2024-06-07 16:48:57 +0800

    TDX api

---
+++ src/app/Components/Card.jsx
@@ -0,0 +1,11 @@
+/*
+  author:blaketow1456@gmail.com
+*/
+export default function CustomCard(item){
+    return(
+        <div class="border">
+            <div>{item.ScenicSpotName}</div>
+
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
 
+/** 
+*   author RoseNFan authorblaketow1456@gmail.com 
+**/
+'use client'
 import Link from "next/link";
 import { 
   Navbar, 
@@ -9,30 +13,58 @@ import {
   Footer ,FooterCopyright, FooterLink, FooterLinkGroup,
   Carousel,
   Button, 
-  Card
+  Card,
+  DarkThemeToggle
 } from "flowbite-react";
-
+import { useState,useEffect } from "react";
+import CustomCard from "./Components/Card";
 export default function Home() {
+  const [items,SetItems]= useState([]);
+  const tokenUrl = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
+  const apiUrl ="https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/YunlinCounty";
+  useEffect(() =>{
+    const getToken = async () => {
+    const clientId=process.env.TDX_CLIENT_ID;
+    const clientSecret=process.env.TDX_CLIENT_SECRET;
+    const tokenParms= new URLSearchParams();
+    tokenParms.append('grant_type','client_credentials');
+    tokenParms.append('client_id',clientId);
+    tokenParms.append('client_secret',clientSecret);
+    const tokenResponse = await fetch(tokenUrl,{
+      method: 'POST',
+      headersL:{
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      body: tokenParms.toString()
+    });
+    };
+  },[]);
+  
   return (
     <>
-    <Navbar fluid rounded>
-      <NavbarBrand as={Link} href="https://flowbite-react.com">
-        <img src="/next.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+    <div className="bg-teal-600">
+    <div className="container mx-auto">
+    <Navbar fluid className="bg-teal-600">
+    
+      <NavbarBrand as={Link} href="/">
+        <img src="https://www.yuntech.edu.tw/images/website_png/Group_640.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="text-white self-center whitespace-nowrap text-xl font-semibold dark:text-white">雲科</span>
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink href="#" active>
-          Home
+        <NavbarLink className="text-white " href="#">
+          <span className="hover:text-black hover:p-0 hover:border-b">Home</span>
+        </NavbarLink >
+        <NavbarLink className="text-white" as={Link} href="#">
+        <span className="hover:text-black hover:border-b">關於</span>
         </NavbarLink>
-        <NavbarLink as={Link} href="#">
-          About
-        </NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Pricing</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
+        <NavbarLink className="text-white" href="#"><span className="hover:text-black hover:border-b">交通</span></NavbarLink>
+        <DarkThemeToggle/>
       </NavbarCollapse>
+      
     </Navbar>
+    </div>
+    </div>
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
         <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
@@ -42,6 +74,8 @@ export default function Home() {
         <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
       </Carousel>
     </div>
+    <div className="bg-white">
+    <div className="container mx-auto">
     <Card className="max-w-sm">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Noteworthy technology acquisitions 2021
@@ -60,6 +94,8 @@ export default function Home() {
         </svg>
       </Button>
     </Card>
+    </div>
+    </div>
     <Footer container>
       <FooterCopyright href="#" by="Flowbite™" year={2022} />
       <FooterLinkGroup>
